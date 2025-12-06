e95382aeec54f9f4ce74cedfb7997c186f812179
v1niciuscarvalh0 <viniciuscarvalhozga@gmail.com>
2025-12-06 20:40:51 -0300

    Delete imobiliaria directory

--- imobiliaria/src/services/errors/UnauthorizedError.js
+++
@@ -1,9 +0,0 @@
-import ApiError from "./ApiError";
-
-class UnauthorizedError extends ApiError {
-  constructor(message = "Unauthorized", status = 401, details = null) {
-    super(message, status, details);
-  }
-}
-
-export default UnauthorizedError;
--- imobiliaria/src/services/errors/UnexpectedError.js
+++
@@ -1,9 +0,0 @@
-import ApiError from "./ApiError";
-
-class UnexpectedError extends ApiError {
-  constructor(message = "Unexpected server error", status = 500, details = null) {
-    super(message, status, details);
-  }
-}
-
-export default UnexpectedError;
